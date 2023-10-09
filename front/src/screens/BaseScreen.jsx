8d50fb96ee92968f706ac6d80296af4ca1cf580b
Bryan <bryan.ferrando@epitech.eu>
2023-10-09 13:13:38 +0100

    Base screen

---
+++ front/src/screens/BaseScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+
+const BaseScreen = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default BaseScreen;
---
+++ front/src/screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1 className='text-blue-500'>Home</h1>
+    </>
+  );
+};
+
+export default HomeScreen;
