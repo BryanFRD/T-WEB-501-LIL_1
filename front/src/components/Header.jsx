801bff07c48e174f2fa87f14ff3757a062e2272d
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-09 16:02:09 +0200

    test

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const Header = () => {
   return (
     <header>
       <h1>JobHub</h1>
+      <Navbar></Navbar>
     </header>
   );
 };
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Button from './Button';
 
 const Navbar = () => {
     return (
         <nav>
-            
+          <ul>
+            <li>Lancer la recherche</li>
+            <li>Déposer une annonce</li>
+            </ul>
+            <Button>Se connecter</Button>
+            <Button>S'inscrire</Button>
         </nav>
     );
 };
