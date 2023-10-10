73f4070f3e492e7b882f47115c33b362668c0e37
Bryan <bryan.ferrando@epitech.eu>
2023-10-10 07:25:20 +0100

    Added AdContractType model

--- back/models/userdata.js
+++ back/models/userdata.js
@@ -4,9 +4,9 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class UserData extends Model {
     static associate(models) {
-      UserData.belongsTo(models.Client, {constraints: false, foreignKey: 'userDataId', as: 'client'});
-      UserData.belongsTo(models.Company, {constraints: false, foreignKey: 'userDataId', as: 'company'});
-      UserData.belongsTo(models.Admin, {constraints: false, foreignKey: 'userDataId', as: 'admin'});
+      UserData.belongsTo(models.Client, {constraints: false, foreignKey: 'associatedId', as: 'client'});
+      UserData.belongsTo(models.Company, {constraints: false, foreignKey: 'associatedId', as: 'company'});
+      UserData.belongsTo(models.Admin, {constraints: false, foreignKey: 'associatedId', as: 'admin'});
     }
   }
   UserData.init({
