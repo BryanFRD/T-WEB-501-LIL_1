e91df7eef191b4d7c50ae136022f837547c5b01c
Bryan <bryan.ferrando@epitech.eu>
2023-10-11 11:24:25 +0100

    Updated auth routes

--- back/config/auth.js
+++ back/config/auth.js
@@ -2,12 +2,12 @@ const isSelf = (body, user) => body?.model?.id === user.id;
 
 const config = {
   RESTRICTED_ROUTES: {
-    'GET/usercredential': ({user, body}) => isSelf(body, user) || isAdmin(user),
-    'PUT/usercredential': ({user, body}) => isSelf(body, user) || isAdmin(user),
-    'DELETE/usercredential': ({user, body}) => isSelf(body, user) || isAdmin(user),
-    'POST/useraccount': ({user}) => isAdmin(user),
-    'PUT/useraccount': ({user, body}) => isSelf(body, user) || isAdmin(user),
-    'DELETE/useraccount': ({user, body}) => isSelf(body, user) || isAdmin(user)
+    'GET/usercredential': ({user, body}) => isSelf(body, user) || user.isAdmin,
+    'PUT/usercredential': ({user, body}) => isSelf(body, user) || user.isAdmin,
+    'DELETE/usercredential': ({user, body}) => isSelf(body, user) || user.isAdmin,
+    'POST/useraccount': ({user}) => user.isAdmin,
+    'PUT/useraccount': ({user, body}) => isSelf(body, user) || user.isAdmin,
+    'DELETE/useraccount': ({user, body}) => isSelf(body, user) || user.isAdmin
   }
 }
 
