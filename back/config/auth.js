278d44fbc1603949d15105ab1ad1e327e3cec318
Bryan <bryan.ferrando@epitech.eu>
2023-10-11 11:18:43 +0100

    Added middlewares

---
+++ back/config/auth.js
@@ -0,0 +1,14 @@
+const isSelf = (body, user) => body?.model?.id === user.id;
+
+const config = {
+  RESTRICTED_ROUTES: {
+    'GET/usercredential': ({user, body}) => isSelf(body, user) || isAdmin(user),
+    'PUT/usercredential': ({user, body}) => isSelf(body, user) || isAdmin(user),
+    'DELETE/usercredential': ({user, body}) => isSelf(body, user) || isAdmin(user),
+    'POST/useraccount': ({user}) => isAdmin(user),
+    'PUT/useraccount': ({user, body}) => isSelf(body, user) || isAdmin(user),
+    'DELETE/useraccount': ({user, body}) => isSelf(body, user) || isAdmin(user)
+  }
+}
+
+module.exports = config;
