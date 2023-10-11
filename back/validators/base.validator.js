6277beff35b6de87dff6f386cd3c3356b8b38ed2
Bryan <bryan.ferrando@epitech.eu>
2023-10-11 07:15:38 +0100

    Changed error message

--- back/validators/base.validator.js
+++ back/validators/base.validator.js
@@ -2,8 +2,14 @@ const Joi = require('joi');
 
 class BaseValidator {
   
-  validate = (schema, data, options = {stripUnknown: true}) => {
-    return schema.validate(data, options);
+  validate = (schema, data, options = {stripUnknown: true, abortEarly: false}) => {
+    const response = schema.validate(data, options);
+    
+    if(response.error){
+      response.error.details = response.error.details[0].context?.details?.map(({context, message}) => ({key: context.key, message}));
+    }
+    
+    return response;
   }
   
 }
