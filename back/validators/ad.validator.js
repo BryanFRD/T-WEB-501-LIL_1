81288a91d278f2caf0e3ec45573b37196a2e92d5
Bryan <bryan.ferrando@epitech.eu>
2023-10-12 11:12:00 +0100

    Added validators

--- back/validators/ad.validator.js
+++ back/validators/ad.validator.js
@@ -3,6 +3,29 @@ const BaseValidator = require('./base.validator');
 
 class AdValidator extends BaseValidator {
   
+  #create = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    status: Joi.string().default('active'),
+    contractType: Joi.array().items(Joi.string()).required(),
+    companyId: Joi.string().uuid().required(),
+  }).required();
+  
+  #update = Joi.object({
+    title: Joi.string(),
+    description: Joi.string(),
+    status: Joi.string(),
+    contactType: Joi.array().items(Joi.string()),
+  }).required();
+  
+  validateCreate = (data, options) => {
+    return this.validate(this.#create, data, options);
+  }
+  
+  validateUpdate = (data, options) => {
+    return this.validate(this.#update, data, options);
+  }
+  
 }
 
 module.exports = AdValidator;
