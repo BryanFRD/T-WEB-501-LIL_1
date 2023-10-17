325f0917bb8993e159357cab4d9eef4592219005
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-17 10:47:14 +0200

    update front mardi 10h46

--- front/src/screens/DisplayDetailsScreen.jsx
+++ front/src/screens/DisplayDetailsScreen.jsx
@@ -1,5 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { UserContext } from '../contexts/UserContext';
+import { Link, Route, Routes, useParams} from 'react-router-dom';
+import Modal from '../components/Modal';
+import ApplyingForm from '../components/form/ApplyingForm';
 
 const TestData2 = {
     id: 1, 
@@ -15,52 +18,86 @@ const TestData2 = {
 
 const DisplayDetailsScreen = () => {
 
-    const {user} = useContext(UserContext)
+    const {id} = useParams(); 
+
+    const {user} = useContext(UserContext); 
+    const [showModal, setShowModal] = useState(false); 
+
+    const openModal = () =>  {
+        setShowModal(true); 
+    }; 
+
+    const closeModal = () => {
+        setShowModal(false); 
+    }
 
     return (
+
         <div className='shadow appearance-none border rounded text-dark leading-tight focus:outline-none focus:shadow-outline
         w-11/12 flex flex-col ml-auto mr-auto mt-6'>
+            
             <div className='ml-auto mr-auto'>
                 <h1 className='mt-3 mb-6 text-2xl'><strong>{TestData2.title}</strong></h1>
             </div>
+            
             <div className='text-left pl-3 pr-3 indent-4'>
                 <p>{TestData2.description}</p>
             </div>
+            
             <div className='pt-5 pb-5 flex flex-col gap-y-4'>
                 <div className='flex flex-row gap-x-2 pl-3'>
                     <p className='underline font-bold'>Type de contrat : </p>
                     <p>{TestData2.contractTypes}</p>
                 </div>
+                
                 <div className='flex flex-row gap-x-2 pl-3'>
                     <p className='underline font-bold'>Rémunération : </p>
                     <p>{TestData2.wages}</p>
                 </div>
+                
                 <div className='flex flex-row gap-x-2 pl-3'>
                     <p className='underline font-bold'>Lieu : </p>
                     <p>{TestData2.place}</p>
                 </div>
+                
                 <div className='flex flex-row gap-x-2 pl-3'>
                     <p className='underline font-bold'>Temps de travail : </p>
                     <p>{TestData2.workingTime}</p>
                 </div>
+                
                 <div className='flex flex-row gap-x-2 pl-3'>
                     <p className='underline font-bold'>Candidatures : </p>
                     <p>{TestData2.status}</p>
                 </div>
+            
             </div>
+            
             <div className='flex flex-row justify-end pr-3 gap-x-2 pt-5 pb-5'>
                 <p className='underline'>Annonce postée :</p>
                 <p className='italic'>{TestData2.createdAt}</p>
             </div>
+
             <div className='flex flex-row gap-5 pb-6 pt-6 ml-auto mr-auto'>
-                <button className='border-2 rounded p-3 border-solid border-black
-                hover:bg-black hover:text-white'>Candidater</button>
+
+                {!user?.name && <button onClick={openModal} 
+                className='border-2 rounded p-3 border-solid border-black
+                hover:bg-black hover:text-white'>Candidater</button>}
+
+                    {showModal && (
+                        <Modal show={showModal} setShow={closeModal} title="Candidater">
+                            <ApplyingForm adId={id} />
+                        </Modal>
+                    )}
 
                 {user?.name && <button className='rounded p-3 text-white bg-black
                 hover:border-black hover:border-solid hover:border-2 hover:bg-white hover:text-black'>Modifier</button>}
+            
             </div>
+        
         </div>
+    
     );
+
 };
 
 export default DisplayDetailsScreen;
