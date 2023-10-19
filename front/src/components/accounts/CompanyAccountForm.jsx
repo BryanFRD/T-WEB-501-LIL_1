22b6e035aca2bdefcd3d7c4fb45e02de574da818
Bryan <bryan.ferrando@epitech.eu>
2023-10-19 14:31:16 +0100

    Update

--- front/src/components/accounts/CompanyAccountForm.jsx
+++ front/src/components/accounts/CompanyAccountForm.jsx
@@ -5,11 +5,11 @@ const CompanyAccountForm = ({handleSubmit, data, handleInputChange}) => {
     <form className='w-3/4 flex flex-col gap-8' onSubmit={handleSubmit}>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4'>
         <label className='md:whitespace-nowrap'>Nom de l'entreprise:</label>
-        <input type="text" className='border-b-2 border-primary rounded bg-slate-200 py-1 px-2 md:col-span-2 lg:col-span-4' value={data?.name} placeholder='Nom' name='name' onChange={handleInputChange}/>
+        <input type="text" className='border-b-2 border-primary rounded bg-slate-200 py-1 px-2 md:col-span-2 lg:col-span-4' value={data?.name} placeholder='Nom de votre entreprise' name='name' onChange={handleInputChange}/>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4'>
         <label className=''>Description:</label>
-        <input type="text" className='border-b-2 border-primary rounded bg-slate-200 py-1 px-2 md:col-span-2 lg:col-span-4' value={data?.description} placeholder='Description' name='description' onChange={handleInputChange}/>
+        <textarea type="text" className='resize-none border-b-2 border-primary rounded bg-slate-200 py-1 px-2 md:col-span-2 lg:col-span-4' value={data?.description} placeholder='Description' name='description' onChange={handleInputChange}/>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4'>
         <label className='md:whitespace-nowrap'>Téléphone:</label>
